fdf4b1427c212fdd793b5634d143cc9b16caceda
Christian <christian@hammervig.dk>
2019-09-02 22:00:29 +0200

    selector

---
+++ src/components/lists.js
@@ -0,0 +1,32 @@
+export const HEAR_LIST = [
+    {
+        id: 1,
+        option: 'Google search'
+    },
+    {
+        id: 2,
+        option: 'From a friend'
+    },
+    {
+        id: 3,
+        option: 'Facebook'
+    },
+    {
+        id: 4,
+        option: 'Instagram'
+    },
+    {
+        id: 5,
+        option: 'Other'
+    }
+];
+/*export const YES_NO = [
+    {
+        id: 1,
+        choice: Yes
+    },
+    {
+        id: 2,
+        choice: No
+    }
+]*/
--- src/components/page/Mail.js
+++ src/components/page/Mail.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { reduxForm } from 'redux-form'
-import { Textarea, Textbox, Select, Checkbox, Radiobox } from 'react-inputs-validation';
+import { Textarea, Textbox, Select } from 'react-inputs-validation';
+import { HEAR_LIST } from '../lists';
 import 'react-inputs-validation/lib/react-inputs-validation.min.css';
 
 class Mail extends React.Component {
     constructor(props){
         super(props);
-        
+        this.handleHearChange = this.handleHearChange.bind(this)
         this.state = {
+                id: '',
                 name: '',
+                option: 'volvo',
                 email: '',
                 description: '',
-                size: this.props.size
+                size: this.props.size,
+                HEAR_LIST,
+                
             }
             this.submitted = false;
     }
@@ -42,9 +47,17 @@ class Mail extends React.Component {
     onSubmit = formValues => {
         this.props.onSubmit(formValues);
     }
+
+    handleHearChange(event){
+        console.log(event);
+        this.setState({
+            id: event.target.value,
+        });
+        
+    }
     
     render(){
-        console.log(this.state.size)
+        
         return (
             <div className="ui container">
                 <article className="container">
@@ -81,30 +94,83 @@ class Mail extends React.Component {
                                     }}
                                 />
 							</div>
+                            <div>
+                            {/*<Radiobox
+                                tabIndex={2} //Optional.[String or Number].Default: none.
+                                id="job" //Optional.[String].Default: "".  Input ID.
+                                name="job" //Optional.[String].Default: "". Input name.
+                                disabled={false} //Optional.[Bool].Default: false.
+                                //value={job} //Optional.[String].Default: "".
+                                //validate={validate} //Optional.[Bool].Default: false. If you have a submit button and trying to validate all the inputs of your form at once, toggle it to true, then it will validate the field and pass the result via the "validationCallback" you provide.
+                                validationCallback={res =>
+                                    this.setState({ hasJobError: res, validate: false })} //Optional.[Func].Default: none. Return the validation result.
+                                //optionList={JOB_OPTIONS_LIST}
+                                classNameInput="" //Optional.[String].Default: "".
+                                classNameWrapper="" //Optional.[String].Default: "".
+                                classNameContainer="" //Optional.[String].Default: "".
+                                classNameOptionListItem="" //Optional.[String].Default: "".
+                                customStyleInput={{}} //Optional.[Object].Default: {}.
+                                customStyleWrapper={{}} //Optional.[Object].Default: {}.
+                                customStyleContainer={{
+                                    display: 'flex',
+                                    justifyContent: 'flex-start'
+                                }} //Optional.[Object].Default: {}.
+                                customStyleOptionListItem={{ marginRight: '20px' }} //Optional.[Object].Default: {}.
+                                onChange={(job, e) =>{
+                                    this.setState({ job });
+                                    console.log(e);
+                                }} //Required.[Func].Default: () => {}. Will return the value.
+                                onBlur={(e) => {console.log(e)}} //Optional.[Func].Default: none.
+                                // onFocus={(e) => {console.log(e)}} //Optional.[Func].Default: none.
+                                // onClick={(e) => {console.log(e)}} //Optional.[Func].Default: none.
+                                validationOption={{
+                                    name: 'Name', //Optional.[String].Default: "". To display in the Error message. i.e Please enter your ${name}.
+                                    check: true, //Optional.[Bool].Default: true. To determin if you need to validate.
+                                    required: true, //Optional.[Bool].Default: true. To determin if it is a required field.
+                                    // showMsg: true, //Optional.[Bool].Default: true. To determin display the error message or not.
+                                    // locale: 'en-US', //Optional.[String].Default: "en-US". For error message display. Current options are ['zh-CN', 'en-US']; Default is 'en-US'. If your are looking for more options, you can take a look at 'window.REACT_INPUTS_VALIDATION' section, which provides the extensibility for your own locale.
+                                    // msgOnError: "Your custom error message if you provide the validationOption['msgOnError']", //Optional.[String].Default: "". Show your custom error message no matter what when it has error if it is provied.
+                                    // msgOnSuccess: "Your custom success message if you provide the validationOption['msgOnSuccess']. Otherwise, it will not show, not even green border." //Optional.[String].Default: "". Show your custom success message no matter what when it has error if it is provied.
+                                }}
+                                // asyncMsgObj={{
+                                //   error: false, // Optional.[Bool].Default: false. (Server response) Backend validation result.
+                                //   message: '', // Optional.[String].Default: "". (Server response) Your AJAX message. For instance, provide it when backend returns 'USERNAME ALREADY EXIST'
+                                //   showOnError: true, // Optional.[Bool].Default: true. (Server response) Show AJAX error message or not.
+                                //   showOnSuccess: false, // Optional.[Bool].Default: false. (Server response) Show AJAX success message or not.
+                                // }}
+                            />*/}
+                            </div>
+                            <div>
+                            <label htmlFor="" className="">Where did hear about this page</label>
+                            <select >
+                                <option >Google search</option>
+                                <option >Volvo</option>
+                                <option >Fiat</option>
+                            </select>
+                            </div>
 							<div className="">
 								<label htmlFor="" className="">Email</label>
 								<Textbox
-                                    tabIndex="1" //Optional.[String or Number].Default: none.
-                                    id={'Email'} //Optional.[String].Default: "".  Input ID.
-                                    name="Email" //Optional.[String].Default: "". Input name.
-                                    type="text" //Optional.[String].Default: "text". Input type [text, password, number].
-                                    disabled={false} //Optional.[Bool].Default: false.
-                                    //maxLength={50} //Optional.[String].Default: "".
-                                    placeholder="Place your email here" //Optional.[String].Default: "".
+                                    tabIndex="1" 
+                                    id={'Email'}
+                                    name="Email"
+                                    type="text"
+                                    disabled={false}
+                                    placeholder="Place your email here"
                                     validationCallback={res =>
                                         this.setState({ hasNameError: res, validate: false })}
                                     onChange={(email, e) => {
                                         this.setState({ email });
                                         console.log(e);
-                                    }} //Required.[Func].Default: () => {}. Will return the value.
-                                    onBlur={(e) => {console.log(e)}} //Optional.[Func].Default: none. In order to validate the value on blur, you MUST provide a function, even if it is an empty function. Missing this, the validation on blur will not work.
-                                    onFocus={(e) => {console.log(e)}} //Optional.[Func].Default: none.
+                                    }}
+                                    onBlur={(e) => {console.log(e)}}
+                                    onFocus={(e) => {console.log(e)}}
                                     validationOption={{
-                                        min: 5, //Optional.[Number].Default: 0. Validation of min length when validationOption['type'] is string, min amount when validationOption['type'] is number.
-                                        max: 50, //Optional.[Number].Default: 0. Validation of max length when validationOption['type'] is string, max amount when validationOption['type'] is number.
-                                        name: 'Email', //Optional.[String].Default: "". To display in the Error message. i.e Please enter your ${name}.
-                                        check: true, //Optional.[Bool].Default: true. To determin if you need to validate.,
-                                        required: true, //Optional.[Bool].Default: true. To determin if it is a required field.
+                                        min: 5,
+                                        max: 50,
+                                        name: 'Email',
+                                        check: true,
+                                        required: true,
                                         customFunc: email => {
                                           const reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
                                           if (reg.test(String(email).toLowerCase())) {
@@ -127,7 +193,7 @@ class Mail extends React.Component {
                                         this.setState({ description });
                                         console.log(e);
                                     }}
-                                    onBlur={(e) => {console.log(e)}} // Optional.[Func].Default: none. In order to validate the value on blur, you MUST provide a function, even if it is an empty function. Missing this, the validation on blur will not work.
+                                    onBlur={(e) => {console.log(e)}}
                                     validationOption={{
                                         name: 'Description',
                                         check: true,

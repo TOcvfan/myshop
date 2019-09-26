f0cde2ee6744ce2fbb6643af2103552540ed056e
TOcvfan <christian@hammervig.dk>
2019-09-26 10:14:31 +0200

    table

---
+++ src/components/page/table.js
@@ -0,0 +1,41 @@
+<table id="domainpack">
+                    <thead>
+                        <tr>
+                            <th>Domain packeges</th>
+                            <th>none</th>
+                            <th style={{backgroundColor: '#b08d57'}}>Bronze</th>
+                            <th style={{backgroundColor: '#aaa9ad'}}>Silver</th>
+                            <th style={{backgroundColor: '#d4af37'}}>Gold</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>own your domain</td>
+                            <td>yes</td>
+                            <td>yes</td>
+                            <td>yes</td>
+                            <td>yes</td>
+                        </tr>
+                        <tr>
+                            <td>24 hour service</td>
+                            <td>no</td>
+                            <td>no</td>
+                            <td>yes</td>
+                            <td>yes</td>
+                        </tr>
+                        <tr>
+                            <td>webhotel and mail</td>
+                            <td>your choice</td>
+                            <td>10 GB</td>
+                            <td>20 GB</td>
+                            <td>50 GB</td>
+                        </tr>
+                        <tr>
+                            <td>saftycertificate</td>
+                            <td>maybe</td>
+                            <td>yes</td>
+                            <td>yes</td>
+                            <td>yes</td>
+                        </tr>    
+                    </tbody>
+                </table>
